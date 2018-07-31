803f886907baab7926c91fc6d176a37762e511fb
Xu_ziheng <1171001052@cnu.edu.cn>
2018-07-31 20:56:44 +0800

    setting introduce

--- setting.py
+++ setting.py
@@ -1,16 +1,17 @@
 
 
+#多个要逗号分隔
 
-
-urls_person = [
-    #'https://www.meitulu.com/t/una-youna/'
+urls_person = [#例如： 'https://www.meitulu.com/t/una-youna/'
+    
 
 
 
 ]
 
-urls_item = [
+urls_item = [#例如 ： 'https://www.meitulu.com/item/8666.html'
+    
     'https://www.meitulu.com/item/7584.html',
-    #'https://www.meitulu.com/item/8666.html',
+    
 
 ]
